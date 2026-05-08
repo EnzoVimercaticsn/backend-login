84b4bdfae9cce12b90c79cdb9e8c72943f62c13f
EnzoVimercaticsn <enzo.vimercati@csn.com.br>
2026-05-08 15:19:31 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,14 @@ const routes = express.Router();
 // Rota Raiz
 routes.get('/', raizControllers.raiz);
 
-// Rotas cadastro - GET
-routes.get('/usuarios', cadastroControler.searchcadastroAllCod);
-routes.get('/usuarios/nome', cadastroControler.searchcadastroAllNome);
-routes.get('/usuarios/buscar/:nome', cadastroControler.searchcadastroByNome);
-
-// Rotas cadastro - POST, PUT, DELETE
+// Rotas cadastro - POST, PUT, DELETE (ANTES)
 routes.post('/usuarios', cadastroControler.createcadastro);
 routes.put('/usuarios/:usu_matricula', cadastroControler.updatecadastro);
 routes.delete('/usuarios/:usu_matricula', cadastroControler.deletecadastro);
+
+// Rotas cadastro - GET (DEPOIS)
+routes.get('/usuarios', cadastroControler.searchcadastroAllCod);
+routes.get('/usuarios/nome', cadastroControler.searchcadastroAllNome);
+routes.get('/usuarios/buscar/:nome', cadastroControler.searchcadastroByNome);
 
 module.exports = routes;
