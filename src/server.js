767da3e77db72c500fcdee0b044b1c0910e71310
EnzoVimercaticsn <enzo.vimercati@csn.com.br>
2026-05-12 14:58:02 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,24 +3,8 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-
-// Configuração CORS robusta
-const corsOptions = {
-  origin: ['http://127.0.0.1:5501', 'http://localhost:5501', 'http://localhost:3000', 'http://localhost:5500'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Headers adicionais de segurança (opcional)
-app.use((req, res, next) => {
-  res.header('X-Content-Type-Options', 'nosniff');
-  next();
-});
-
 app.use(routes)
 
 const PORT = process.env.PORT || 3344;
