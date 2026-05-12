83d970a1fd47eead9bd8b8d63930235125f7f487
EnzoVimercaticsn <enzo.vimercati@csn.com.br>
2026-05-12 14:20:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(routes)
 
 const PORT = process.env.PORT || 3344;
